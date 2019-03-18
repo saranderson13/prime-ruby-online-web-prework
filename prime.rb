2d61511df51d010c1c66d540055d4588f789807a
Sarah Anderson <saranderson13@gmail.com>
2019-03-18 23:39:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(number)
+  breaks_prime_rules = false
+  case 
+    when number <= 1
+      breaks_prime_rules = true
+    when number == 2
+      breaks_prime_rules = false
+    when number > 2
+      array_range = (2..number - 1).to_a
+      array_range.each do |divide_by|
+        if number % divide_by == 0
+          breaks_prime_rules = true
+          break
+        else
+          breaks_prime_rules = false
+        end
+      end
+  end
+  
+  breaks_prime_rules == false ? true : false
+end
