590a60ae80b2442acc9adad03ecec3ccb4e4565f
Sarah Anderson <saranderson13@gmail.com>
2019-03-19 13:56:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,6 @@
 # Add  code here!
 def prime?(number)
+<<<<<<< HEAD
   breaks_prime_rules = false
   case 
     when number <= 1
@@ -19,4 +20,19 @@ def prime?(number)
   end
   
   breaks_prime_rules == false ? true : false
+=======
+
+  is_negative = number < 1 ? true : false
+  is_prime = false
+  
+  range_limit = number > 2 ? (number - 1) : 2
+  array_range = (2..range_limit).to_a
+  array_range.each do |divide_by|
+    is_prime = number % divide_by == 0 ? true : false
+    puts divide_by
+    is_prime == true ? true : false
+  end
+  
+  is_negative != true && is_prime == true ? true : false
+>>>>>>> 74c6b55ad944a68f6395f9b2ec3d4662fd34aabd
 end
